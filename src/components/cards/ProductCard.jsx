83e827d2cfcef32947963062b2d1d1300d56f10a
Talha Irtisam <talha.ultracodes@gmail.com>
2025-01-14 22:11:22 +0500

    card size

--- src/components/cards/ProductCard.jsx
+++ src/components/cards/ProductCard.jsx
@@ -2,7 +2,7 @@ import { WalletIcon } from "@heroicons/react/24/solid";
 
 const ProductCard = ({ title, price, image }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden w-64">
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden w-32 sm:w-64 flex-shrink-0">
       {/* Image Section */}
       <img
         src={image} // Replace with your actual image URL
@@ -13,11 +13,11 @@ const ProductCard = ({ title, price, image }) => {
       />
 
       {/* Content Section */}
-      <div className="p-4 flex align-middle justify-between">
-        <h3 className="text-lg font-bold text-gray-800">{title}</h3>
+      <div className="p-1 sm:p-4 flex flex-col sm:flex-row align-middle justify-between">
+        <h3 className="text-xs sm:text-lg font-bold text-gray-800">{title}</h3>
         <div className="flex items-center">
-          <WalletIcon className="h-5 w-5 text-blue-500" />
-          <span className="text-blue-500 font-medium text-lg ml-1">${price}</span>
+          <WalletIcon className="h-3 w-3 sm:h-5 sm:w-5 text-blue-500" />
+          <span className="text-blue-500 font-medium text-xs sm:text-lg ml-1">${price}</span>
         </div>
       </div>
 
