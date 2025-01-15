21d6271efc20410918e3db332a1b5767c7531464
Talha Irtisam <talha.ultracodes@gmail.com>
2025-01-15 15:51:37 +0500

    fix title

--- src/components/featuredNFT/FeaturedNFT.jsx
+++ src/components/featuredNFT/FeaturedNFT.jsx
@@ -6,7 +6,7 @@ import nfts from "../../data/nfts";
 const FeatureNFT = () => {
   return (
     <div className="p-4">
-      <p className="text-2xl text-center font-bold pb-4">Our Featured NFTs</p>
+      <p className="text-2xl text-center font-bold pb-4">Our Featured GFTs</p>
       <div className="flex flex-wrap gap-4 justify-center">
         {nfts.map((nft) => (
           <ProductCard key={nft.id} {...nft} />
